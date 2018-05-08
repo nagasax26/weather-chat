b3b21b1fe0ea1372f7e85146204677ac0cda6a98
nagasax26 <smarts003@gmail.com>
2018-05-08 13:53:37 +0300

    weather chat app

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+
+//static file
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
+
+
+//pages url
+app.get('/', function (req, res) {
+    res.sendFile('index');
+});
+
+
+
+
+
+
+
+//listening on port
+const port = process.env.port || 3000;
+app.listen(port, function () {
+    console.log('server listening on port', port);
+});
